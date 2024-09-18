02402bdeaaaa7d9b47698eb1141b409c157efdf7
Saina Firooz <saina.firooz@gmail.com>
2024-09-18 23:00:58 +0200

    contact section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
 import { AboutSection } from "@/sections/About";
 import { TestimonialsSection } from "@/sections/Testimonials";
+import { ContactSection } from "@/sections/Contact";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <TapeSection />
       <TestimonialsSection />
       <AboutSection />
+      <ContactSection />
     </div>
   );
 }
