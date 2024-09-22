4c8004d2af0d6b4880f1def370e281834010b14e
Saina Firooz <saina.firooz@gmail.com>
2024-09-23 00:58:04 +0200

    small changes,

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -2,19 +2,24 @@
 import { SectionHeader } from "@/components/SectionHeader";
 import { Card } from "@/components/Card";
 import bookImage from "@/assets/images/book-cover.png";
+import movieImage from "@/assets/images/poorthings.jpg";
 import Image from "next/image";
 import JavascriptIcon from "@/assets/icons/square-js.svg";
 import HTMLIcon from "@/assets/icons/html5.svg";
 import CssIcon from "@/assets/icons/css3.svg";
 import ReactIcon from "@/assets/icons/react.svg";
-import ChromeIcon from "@/assets/icons/chrome.svg";
+import PhpIcon from "@/assets/icons/PHP.svg";
 import GithubIcon from "@/assets/icons/github.svg";
-import mapImage from "@/assets/images/map.png";
-import memojiImage from "@/assets/images/memoji-avatar-4.png";
+import mapImage from "@/assets/images/stockholm.png";
+import me from "@/assets/images/me.png";
 import { CardHeader } from "@/components/CardHeader";
 import { ToolboxItems } from "@/components/ToolboxItems";
 import { motion } from "framer-motion";
 import { useRef } from "react";
+import PythonIcon from "@/assets/icons/python.svg";
+import DockerIcon from "@/assets/icons/docker.svg";
+import TypescriptIcon from "@/assets/icons/typescript.svg";
+import NodeIcon from "@/assets/icons/node.svg";
 
 const toolboxItems = [
   {
@@ -34,12 +39,28 @@ const toolboxItems = [
     iconType: ReactIcon,
   },
   {
-    title: "Chrome",
-    iconType: ChromeIcon,
+    title: "PHP",
+    iconType: PhpIcon,
   },
   {
     title: "Github",
     iconType: GithubIcon,
+  },
+  {
+    title: "Python",
+    iconType: PythonIcon,
+  },
+  {
+    title: "Docker",
+    iconType: DockerIcon,
+  },
+  {
+    title: "Node",
+    iconType: NodeIcon,
+  },
+  {
+    title: "Typescript",
+    iconType: TypescriptIcon,
   },
 ];
 
@@ -79,7 +100,7 @@ const hobbies = [
 export const AboutSection = () => {
   const constraintRef = useRef(null);
   return (
-    <div className="py-20 lg:py-28">
+    <div id="about" className="py-20 lg:py-28">
       <div className="container">
         <SectionHeader
           eyebrow="About Me"
@@ -89,13 +110,10 @@ export const AboutSection = () => {
         <div className="mt-20 flex flex-col gap-8">
           <div className="grid grid-cols-1 gap-8 md:grid md:grid-cols-5  lg:grid-cols-3">
             <Card className="h-[320px] md:col-span-2 lg:col-span-1">
-              <CardHeader
-                title="My Reads"
-                description="Explore the books shaping my perspectives."
-              />
+              <CardHeader title="Films" description="Current favorite:" />
 
               <div className="w-40 mx-auto mt-2 md:mt-0">
-                <Image src={bookImage} alt="Book Cover" />
+                <Image src={movieImage} alt="Book Cover" />
               </div>
             </Card>
             <Card className="h-[320px] md:col-span-3 lg:col-span-2">
@@ -154,7 +172,7 @@ export const AboutSection = () => {
 
                 <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
 
-                <Image src={memojiImage} alt="Me" className="size-20" />
+                <Image src={me} alt="Me" className="size-18 rounded-full" />
               </div>
             </Card>
           </div>
