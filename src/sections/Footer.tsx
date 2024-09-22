ee3893db1d98c59901779d88667153d51cb0b270
Saina Firooz <saina.firooz@gmail.com>
2024-09-23 00:58:25 +0200

    icons changed, git and linkedin

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,41 +1,31 @@
-import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg";
-const footerLinks = [
-  {
-    title: "Youtube",
-    href: "#",
-  },
-  {
-    title: "Twitter",
-    href: "#",
-  },
-  {
-    title: "Instagram",
-    href: "#",
-  },
-  {
-    title: "LinkedIn",
-    href: "#",
-  },
-];
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export const Footer = () => {
   return (
-    <footer className="relative -z-10 overflow-x-clip">
+    <footer className="relative overflow-x-clip">
       <div className="absolute h-[400px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/30 [mask-image:radial-gradient(50%_50%at_bottom_center,black,transparent)] -z-10"></div>
       <div className="container">
         <div className="border-t border-white/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
           <div className="text-white/40">&copy; 2024. All rights reserved.</div>
-          <nav className="flex flex-col md:flex-row items-center gap-8">
-            {footerLinks.map((link) => (
-              <a
-                href={link.href}
-                key={link.title}
-                className="inline-flex items-center gap-1.5"
-              >
-                <span className="font-semibold">{link.title}</span>
-                <ArrowUpRightIcon className="size-4" />
-              </a>
-            ))}
+          <nav className="flex items-center gap-8 justify-start px-40">
+            <a
+              href="https://github.com/SainaFirooz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1.5 text-white hover:text-gray-300"
+            >
+              <FaGithub className="w-8 h-8" />
+              <span className="sr-only">GitHub</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/saina-firooz"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center gap-1.5 text-white hover:text-gray-300"
+            >
+              <FaLinkedin className="w-8 h-8" />
+              <span className="sr-only">LinkedIn</span>
+            </a>
           </nav>
         </div>
       </div>
