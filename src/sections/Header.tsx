10eb3a95781064b7d082cb234be511cf19e7382f
Saina Firooz <saina.firooz@gmail.com>
2024-09-23 00:58:34 +0200

    fixed issues

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,22 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const Header = () => {
+  const pathname = usePathname();
+
+  const isHomePage = pathname === "/";
+
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur">
-        <a href="#" className="nav-item">
+        <Link href={isHomePage ? "#home" : "/"} className="nav-item">
           Home
-        </a>
-        <a href="#" className="nav-item">
+        </Link>
+        <Link
+          href={isHomePage ? "#projects" : "/#projects"}
+          className="nav-item"
+        >
           Projects
-        </a>
-        <a href="#" className="nav-item">
+        </Link>
+        <Link href={isHomePage ? "#about" : "/#about"} className="nav-item">
           About
-        </a>
-        <a
-          href="#"
+        </Link>
+        <Link
+          href={isHomePage ? "#contact" : "/#contact"}
           className="nav-item bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900"
         >
           Contact
-        </a>
+        </Link>
       </nav>
     </div>
   );
